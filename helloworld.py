79efc37c9ea1c3f805554fa0924f28e6a8aed405
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-25 22:25:35 -0700

    added in mean fn to meanbranch

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,12 @@
 print("Hello World! The weather sucks.")
+
+# mean
+def mean(input):
+  tot = 0
+  for entry in input:
+    sum += entry
+  return sum / len(input)
+
+# test mean func
+my_list = [0, 1, 2, 3, 4, 5]
+print(mean(my_list))
