310f07409e34102d66983fc2be7fb0362426d860
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-25 22:50:17 -0700

    fixed vars on median branch

--- helloworld.py
+++ helloworld.py
@@ -8,7 +8,7 @@ def median(input2):
   if len(input2) % 2 == 0:
     return((input2[math.floor(list1 / 2)-1]+input2[math.floor(list1 / 2)])/2)
   else:
-    return (input[math.floor(list1/2)])
+    return (input2[math.floor(list1/2)])
 
 # testing median func
 print(median(my_list))
