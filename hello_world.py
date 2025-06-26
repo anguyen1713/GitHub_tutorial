2eea54d4f5669e3a28ea0d4692dc49630fb62d6a
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-25 21:23:45 -0700

    added mean func to mean branch

--- hello_world.py
+++ hello_world.py
@@ -2,3 +2,13 @@
 print("Hello, world! NO YAY")
 
 
+# mean function
+def mean(input):
+    tot=0
+    for entry in input:
+        tot += entry
+    return tot/len(input)
+
+# test
+my_list = [0, 1, 2, 3, 4, 5]
+print(mean(my_list))
