63f8beb7158f4cf578d7ceddeb6fbfd425dae82f
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-25 22:28:58 -0700

    added median fn to medianbranch

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,14 @@
 print("Hello World! The weather sucks.")
+
+# median func
+import math
+
+def median(input):
+  list1=len(input)
+  if len(input) % 2 == 0:
+    return((input[math.floor(list1 / 2)-1]+input[math.floor(list1 / 2)])/2)
+  else:
+    return (input[math.floor(list1/2)])
+
+# testing median func
+print(median(my_list))
