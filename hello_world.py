7173a941b477584ff62b9c11bf24602c1da80bf2
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-25 21:47:54 -0700

    merges

--- hello_world.py
+++ hello_world.py
@@ -2,15 +2,17 @@
 print("Hello, world! NO YAY")
 
 
-# mean function
-def mean(input):
-    tot=0
-    for entry in input:
-        tot += entry
-    return tot/len(input)
+# median function
+import math
+def median(input):
+  length=len(input)
+  if length % 2 == 0:
+    return((input[math.floor(length / 2)-1]+input[math.floor(length / 2)])/2)
+  else:
+    return (input[math.floor(length/2)])
 
 # test
 my_list = [0, 1, 2, 3, 4, 5]
 print(mean(my_list))
 
-# testing the branch stuff
+# testing the median switch
