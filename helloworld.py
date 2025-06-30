1dc33e0b22112c65761b83d096b989612820c9dc
Andrew Nguyen <andrewnguyen@Andrews-MacBook-Air-3.local>
2025-06-29 22:48:16 -0700

    removed mean

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,2 @@
 print("Hello World! The weather sucks.")
 
-# mean
-def mean(input):
-  tot = 0
-  for entry in input:
-    tot += entry
-  return tot / len(input)
-
-# test mean func
-my_list = [0, 1, 2, 3, 4, 5]
-print(mean(my_list))
